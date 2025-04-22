07debb0e8213212e4d7478e94293501c6b09cb40
Fruugu <tuukka.janonen@gmail.com>
2025-04-22 06:38:10 +0300

    AsyncAPI for IoT-devices

---
+++ utils/db.js
@@ -0,0 +1,29 @@
+﻿
+let database = [
+    { topic: "room/5/temperature/5/measured", id: 5, temperature: 25, sentAt: new Date().toISOString() },
+    { topic: "room/5/temperature/5/measured", id: 5, temperature: 22, sentAt: new Date().toISOString() }
+];
+
+function addData(topic, data) {
+    if (!Array.isArray(data)) {
+        data = [data];
+    }
+
+    data.forEach(item => {
+        if (topic && item.id && item.temperature && item.sentAt) {
+            database.push({
+                ...item,
+                id: item.id,
+                topic: topic,
+                temperature: item.temperature,
+                sentAt: item.sentAt
+            });
+        }
+    });
+}
+
+function getDataByTopic(topic) {
+    return database.filter(item => item.topic === topic);
+}
+
+module.exports = { addData, getDataByTopic };
